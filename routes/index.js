4801daadb7f8e32452876bc1d3f238330bf18cc1
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-24 20:05:55 -0300

    asoosjdj

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 const vehiclesRoutes = require("./vehicles");
-const authRoutes = require("./auth");
-const uploadDataRoutes = require("./uploadData");
-const uploadImageRoutes = require("./uploadFile");
-const usersRoutes = require("./users");
-const postRoutes = require("./post");
-const emailRoutes = require("./email");
+// const authRoutes = require("./auth");
+// const uploadDataRoutes = require("./uploadData");
+// const uploadImageRoutes = require("./uploadFile");
+// const usersRoutes = require("./users");
+// const postRoutes = require("./post");
+// const emailRoutes = require("./email");
 
 router.use("/vehicles", vehiclesRoutes);
-router.use("/auth", authRoutes);
-router.use("/users", usersRoutes);
-router.use("/upload", uploadDataRoutes);
-router.use("/post", postRoutes);
-router.use("/uploadImage", uploadImageRoutes);
-router.use("/email", emailRoutes);
-
+// router.use("/auth", authRoutes);
+// router.use("/users", usersRoutes);
+// router.use("/upload", uploadDataRoutes);
+// router.use("/post", postRoutes);
+// router.use("/uploadImage", uploadImageRoutes);
+// router.use("/email", emailRoutes);
+router.get("/vehicles", async (req, res) => {
+  try {
+    const todos = await Vehicles.find();
+    console.log("Se llamó a la ruta /vehicles");
+    res.json(todos);
+  } catch (error) {
+    console.error("Error al obtener los vehículos", error);
+    res.status(500).json({ error: "Error al obtener los vehículos" });
+  }
+});
 module.exports = router;
