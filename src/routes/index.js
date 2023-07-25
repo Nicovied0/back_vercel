c77d723e4ce4e5c91bed4259bc828f663857543a
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-24 22:19:08 -0300

    dsadoi

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,14 +5,15 @@ const vehiclesRoutes = require("./vehicles");
 const authRoutes = require("./auth");
 const usersRoutes = require("./users");
 // const postRoutes = require("./post");
-// const uploadImageRoutes = require("./uploadFile");
+const uploadImageRoutes = require("./uploadFile");
 // const emailRoutes = require("./email");
 
 router.use("/vehicles", vehiclesRoutes);
 router.use("/auth", authRoutes);
 router.use("/users", usersRoutes);
+
 // router.use("/post", postRoutes);
-// router.use("/uploadImage", uploadImageRoutes);
+router.use("/uploadImage", uploadImageRoutes);
 // router.use("/email", emailRoutes);
 
 module.exports = router;
