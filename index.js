36a27d4cb14f237cea82ea15b715763065f82153
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-24 21:44:43 -0300

    Oisdj

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 // Conexión a la base de datos
-const dbConnect = require("./config/mongo");
+const dbConnect = require("./src/config/mongo");
 
 // Importar las rutas desde la carpeta "routes"
-const routes = require("./routes");
+const routes = require("./src/routes/index");
+app.use("/", routes);
 
 dbConnect().then(
   (res) => {
-    // Usar las rutas en tu aplicación
-    app.use("/", routes);
-
     // Iniciar el servidor
     app.listen(PORT, () => {
       console.log("Successfully connected");
