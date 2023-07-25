147fbe695df51f33f17d1e719fe9e99e7a07fc2d
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-24 22:17:17 -0300

    IKojD

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,16 +4,15 @@ const router = express.Router();
 const vehiclesRoutes = require("./vehicles");
 const authRoutes = require("./auth");
 const usersRoutes = require("./users");
+const postRoutes = require("./post");
 // const uploadImageRoutes = require("./uploadFile");
-// const postRoutes = require("./post");
-const emailRoutes = require("./email");
-const user = require("./json");
+// const emailRoutes = require("./email");
 
 router.use("/vehicles", vehiclesRoutes);
 router.use("/auth", authRoutes);
 router.use("/users", usersRoutes);
-// router.use("/post", postRoutes);
+router.use("/post", postRoutes);
 // router.use("/uploadImage", uploadImageRoutes);
-router.use("/email", emailRoutes);
+// router.use("/email", emailRoutes);
 
 module.exports = router;
