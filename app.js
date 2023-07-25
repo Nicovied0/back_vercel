11cdacf0dd8fd0b79863783d7c5552ef0030bde4
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-24 21:16:52 -0300

    Jdsaj

--- app.js
+++ app.js
@@ -8,49 +8,11 @@ const app = express();
 
 app.use(morgan("dev"));
 
-app.use(cors()); //error de origen cruzado
-app.use(express.json()); //Manejar data .json
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 app.name = "API";
-
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-// app.use(bodyParser.json({ limit: '50mb' }));
-app.use(cookieParser());
-// app.use(morgan('dev'));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // or `http://localhost:${FRONT}`// update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-
-// app.use("/", require("./routes"));
-
-app.get("/hello", (req, res) => {
-  res.send("¡Hola! Esta es una prueba exitosa de Express en Vercel.");
-});
-
-app.use((req, res) => {
-  res.status(404).send("Ruta no encontrada");
-});
-
-// Manejador de errores
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Algo salió mal.");
-});
-
-// Error catching endware.
-app.use((err, req, res, next) => {
-  // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const cors = require("cors");
 const dotenv = require("dotenv");
-const app = require("./app");
+const express = require("express");
+const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
@@ -8,11 +8,18 @@ const PORT = process.env.PORT || 3001;
 // Conexión a la base de datos
 const dbConnect = require("./config/mongo");
 
+// Importar las rutas desde la carpeta "routes"
+const routes = require("./routes");
+
 dbConnect().then(
   (res) => {
+    // Usar las rutas en tu aplicación
+    app.use("/", routes);
+
+    // Iniciar el servidor
     app.listen(PORT, () => {
       console.log("Successfully connected");
-      console.log(`http://localhost:${PORT}`);
+      console.log(`Servidor Express escuchando en el puerto ${PORT}`);
     });
   },
   (error) => {
