f66d2d3c45881dbac2c8f053e6abc8cef490cc15
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-24 22:13:46 -0300

    IJoioji

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,20 +3,18 @@ const router = express.Router();
 
 const vehiclesRoutes = require("./vehicles");
 const authRoutes = require("./auth");
-// const uploadDataRoutes = require("./uploadData");
-// const uploadImageRoutes = require("./uploadFile");
-// const usersRoutes = require("./users");
-// const postRoutes = require("./post");
-// const emailRoutes = require("./email");
+const usersRoutes = require("./users");
+const uploadImageRoutes = require("./uploadFile");
+const postRoutes = require("./post");
+const emailRoutes = require("./email");
 const user = require("./json");
 
 router.use("/vehicles", vehiclesRoutes);
 router.use("/auth", authRoutes);
-// router.use("/users", usersRoutes);
-// router.use("/upload", uploadDataRoutes);
-// router.use("/post", postRoutes);
-// router.use("/uploadImage", uploadImageRoutes);
-// router.use("/email", emailRoutes);
+router.use("/users", usersRoutes);
+router.use("/post", postRoutes);
+router.use("/uploadImage", uploadImageRoutes);
+router.use("/email", emailRoutes);
 router.use("/users2", user);
 
 module.exports = router;
