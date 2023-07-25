e0673609723e0239e8a47a298e91143fc693c9d4
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-24 22:11:00 -0300

    jdo

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,21 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const vehiclesRoutes = require("./vehicles");
-// const authRoutes = require("./auth");
-// const uploadDataRoutes = require("./uploadData");
-// const uploadImageRoutes = require("./uploadFile");
-// const usersRoutes = require("./users");
+const authRoutes = require("./auth");
+const uploadDataRoutes = require("./uploadData");
+const uploadImageRoutes = require("./uploadFile");
+const usersRoutes = require("./users");
 // const postRoutes = require("./post");
-// const emailRoutes = require("./email");
+const emailRoutes = require("./email");
 const user = require("./json");
 
 router.use("/vehicles", vehiclesRoutes);
-// router.use("/auth", authRoutes);
-// router.use("/users", usersRoutes);
-// router.use("/upload", uploadDataRoutes);
+router.use("/auth", authRoutes);
+router.use("/users", usersRoutes);
+router.use("/upload", uploadDataRoutes);
 // router.use("/post", postRoutes);
-// router.use("/uploadImage", uploadImageRoutes);
-// router.use("/email", emailRoutes);
-router.use("/users", user);
+router.use("/uploadImage", uploadImageRoutes);
+router.use("/email", emailRoutes);
+router.use("/users2", user);
 
 module.exports = router;
