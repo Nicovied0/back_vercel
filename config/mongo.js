45025c6c74c4d0a783ff1e766766f4e4b019efa3
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-24 21:33:13 -0300

    IJiodJ

--- config/mongo.js
+++ config/mongo.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const dbConnect = () => {
-  const DB_URI = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.gfac6fk.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`;
+  // const DB_URI = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.gfac6fk.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`;
   // const DB_URI = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.ct1ukb2.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`;
+  
+    const DB_URI = `mongodb+srv://adminBBVV:TPryUkoJef4Rskbu@cluster0.gfac6fk.mongodb.net/app_bomberos?retryWrites=true&w=majority`;
 
   // const DB_URI = "mongodb://localhost:27017/MyDB";
   return mongoose.connect(DB_URI, {
