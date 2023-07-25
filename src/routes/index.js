989eb7aa2f32882e34b70fc68e444bfbf58cbfb9
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-24 22:16:16 -0300

    IKojD

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,16 +5,15 @@ const vehiclesRoutes = require("./vehicles");
 const authRoutes = require("./auth");
 const usersRoutes = require("./users");
 // const uploadImageRoutes = require("./uploadFile");
-const postRoutes = require("./post");
+// const postRoutes = require("./post");
 const emailRoutes = require("./email");
 const user = require("./json");
 
 router.use("/vehicles", vehiclesRoutes);
 router.use("/auth", authRoutes);
 router.use("/users", usersRoutes);
+// router.use("/post", postRoutes);
 // router.use("/uploadImage", uploadImageRoutes);
-router.use("/post", postRoutes);
 router.use("/email", emailRoutes);
-router.use("/users2", user);
 
 module.exports = router;
